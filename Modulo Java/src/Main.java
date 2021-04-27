3e594260f7167677985d5eae443f982a2bff674f
JavierLopez78 <jltoro18@jaumebalmes.net>
2021-04-27 17:39:41 +0200

    He creado un hola mon!

--- Modulo Java/src/Main.java
+++ Modulo Java/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hola mon!");
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Bienvenido al gestor de datos del supermercado Mercadona");
+        System.out.println();
+        System.out.println("Que hacer:");
+        System.out.println("1 = control de stock");
+        System.out.println("2 = ventas");
+        int seleccion = entrada.nextInt();
+        if (seleccion == 1){
+            //Ir hacia stock.
+        }
+        else{
+            //Ir hacia ventas.
+        }
     }
 }
